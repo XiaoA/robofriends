adf66a1567b649cb64462c661228b9ccc5c46bac
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-01 21:03:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## RoboFriends 
+
+This is a simple React app that converts names to robots, using the [RoboHash](https://robohash.org/) API.
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Meredith Walters',
+    username: 'meredith_walters',
+    email: 'meredith.walters@example.com',
+  },
+  {
+    id: 2,
+    name: 'Randal George',
+    username: 'randal_george',
+    email: 'randal.george@example.com',
+  },
+  {
+    id: 3,
+    name: 'Emma Rowe',
+    username: 'emma_rowe',
+    email: 'emma.rowe@example.com',
+  },
+  {
+    id: 4,
+    name: 'Tony Becker',
+    username: 'tony_becker',
+    email: 'tony.becker@example.com',
+  },
+  {
+    id: 5,
+    name: 'Gilbert Hernandez',
+    username: 'gilbert_hernandez',
+    email: 'gilbert.hernandez@example.com',
+  },
+  {
+    id: 6,
+    name: 'Connie Hines',
+    username: 'connie_hines',
+    email: 'connie.hines@example.com',
+  },
+  {
+    id: 7,
+    name: 'Barry Roberson',
+    username: 'barry_roberson',
+    email: 'barry.roberson@example.com',
+  },
+  {
+    id: 8,
+    name: 'Gretchen_Warren',
+    username: 'gretchen_warren',
+    email: 'gretchen.warren@example.com',
+  },
+  {
+    id: 9,
+    name: 'Rhonda_Byrd',
+    username: 'rhonda_byrd',
+    email: 'rhonda.byrd@example.com',
+  },
+  {
+    id: 10,
+    name: 'Caroline Hunt',
+    username: 'caroline_hunt',
+    email: 'caroline.hunt@example.com',
+  }
+];
